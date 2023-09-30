4d4cc667a81484bad6ea97666ecfb77543638044
jinkyeong <wlsrud0303@naver.com~>
2023-09-30 15:18:24 +0900

    fourteenth commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -389,7 +389,7 @@ export default {
 
     // 웹소켓 연결, 성공시 메세지 받기
     connect() {
-      const serverURL = 'https://www.loveyourdog.co.kr/api/ws';
+      const serverURL = 'https://loveyourdog.co.kr/api/ws';
       // const serverURL = 'http://www.loveyourdog.co.kr:80/api/ws';
 
       let socket = new SockJS(serverURL);
